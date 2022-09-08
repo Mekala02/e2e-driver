fd390524ad7671498bd0f2a6a2f9f6a076a6604d
itu-emobility-lab <ituemobility@gmail.com>
2022-09-08 13:22:15 +0300

    delete unnecesary

--- parts/arduino/arduino.ino
+++ parts/arduino/arduino.ino
@@ -1,32 +1,13 @@
-#include <Servo.h>
-#include <Ewma.h>
-
 #define PPM_PIN 3
-#define ESC_PIN 9
-#define Servo_PIN 10
-#define ENCODER_PIN 2
 
-// Exponentially Weighted Moving Average Filter
-Ewma Encoder_Filter(0.15);
-
-Servo ESC;
-Servo SERVO;
-
-String pyserial_data;
-int pyserial_throttle, pyserial_steering;
 int steering, throttle;
 
-// current time, previous time
 unsigned long int t, pt = 0;
-// channel no, time differance
 int c, d = 0;
 int ch[15];
 
 unsigned long current_time;
-unsigned long last_serial;
 unsigned long serialOut_last_time;
-unsigned long encoder_last_time;
-unsigned int pulses = 0;
 float dpulses = 0;
 
 void Read_Channels();
@@ -34,11 +15,7 @@ int mode(int pwm);
 
 void setup() {
     pinMode(PPM_PIN, INPUT_PULLUP);
-    pinMode(ENCODER_PIN, INPUT);
-    ESC.attach(ESC_PIN);
-    SERVO.attach(Servo_PIN);
     attachInterrupt(digitalPinToInterrupt(PPM_PIN), Read_Channels, FALLING);
-    attachInterrupt(digitalPinToInterrupt(ENCODER_PIN), []{pulses++;}, FALLING);
     // Default values for steering and throttle
     ch[1] = 1500;
     ch[3] = 1500;
@@ -47,59 +24,11 @@ void setup() {
 
 void loop() {
     current_time = millis();
-    // Reading serial input
-    // Serial inputs shape: s(float)t(float)e
-    if(Serial.available() > 0){
-        pyserial_data =  Serial.readStringUntil('\r');
-        int deliminator_index = 0;
-        if (pyserial_data[0] == 's'){
-            for (int i = 1; i < pyserial_data.length(); i++) {
-                if (pyserial_data[i] == 't'){
-                    pyserial_steering = pyserial_data.substring(1, i).toInt();
-                    deliminator_index = i;
-                }
-                else if (pyserial_data[i] == 'e'){
-                    pyserial_throttle = pyserial_data.substring(deliminator_index+1, i).toInt();
-                    break;
-                }
-            }
-        }
-        last_serial = current_time;
-    }
-    else{
-        // If python program stoped (no response for 50ms)
-        if (current_time - last_serial > 50){
-            pyserial_steering = 0;
-            pyserial_throttle = 0;
-        }
-    }
 
-    // If value equals to 0 we are in manuel mode
-    if (pyserial_steering == 0 && ch[1] < 2000)
+    if (ch[1] < 2000)
         steering = ch[1];
-    else if (1000 < pyserial_steering && pyserial_steering < 2000)
-        steering = pyserial_steering;
-    
-    if (pyserial_throttle == 0)
-        throttle = ch[3];
-    else if (1000 < pyserial_throttle && pyserial_throttle < 2000)
-        throttle = pyserial_throttle;
-    else
-        throttle = 1500;
 
-    // Arming the esc according to ch7
-    SERVO.writeMicroseconds(steering);
-    if (ch[7] > 1900 && ch[7] < 2100)
-        ESC.writeMicroseconds(throttle);
-    else
-        ESC.writeMicroseconds(1500);
-
-    // Calculating speed at certain rate
-    if (current_time - encoder_last_time > 10){
-        dpulses = Encoder_Filter.filter(1000 * pulses / (current_time - encoder_last_time));
-        pulses = 0;
-        encoder_last_time = current_time;
-    }
+    throttle = ch[3];
 
     // Sending data 100 times per second
     if (current_time - serialOut_last_time > 10){
