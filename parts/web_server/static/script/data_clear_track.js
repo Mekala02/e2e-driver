03da8ba22953a2b27b0be97fb66a1eb164f5be5e
Mehmet Eren Kala <43720526+Mekala02@users.noreply.github.com>
2022-09-13 17:36:47 +0300

    name_change

--- parts/web_server/static/script/data_clear_track.js
+++ parts/web_server/static/script/data_clear_track.js
@@ -13,4 +13,22 @@ class Data_Clear_Track extends Main_Track {
     Update_Data_Position(){
         
     }
+
+    update_graph_display(){
+        this.axis_length = this.graph["Timestamp"].length
+        var traces = []
+        for (let mode of this.outputs["Graph1_Mode"]){
+          traces.push({
+            x: this.graph["Timestamp"].slice(Math.max(0, this.axis_length - 500), this.axis_length - 1),
+            y: this.graph[mode].slice(Math.max(0, this.axis_length - 500), this.axis_length - 1),
+            name: mode,
+            type:'line',
+            line: {
+            width: 4
+          }})
+        }
+        this.graph_layout["xaxis"]["range"] = [this.graph_trace["x"][0], this.graph_trace["x"][500]]
+        Plotly.react("Graph1", traces, this.graph_layout, {displayModeBar: false})
+      }
+      
 }
