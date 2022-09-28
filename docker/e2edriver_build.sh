86e783cff4f8115ce98a4392c8e8c04298959701
Mehmet Eren Kala <43720526+Mekala02@users.noreply.github.com>
2022-09-28 20:27:38 +0300

    version fix

--- docker/e2edriver_build.sh
+++ docker/e2edriver_build.sh
@@ -12,8 +12,8 @@ echo "Building $CONTAINER container..."
 
 sudo docker build --network=host -t e2e-driver -f Dockerfile.e2edriver \
 		--build-arg BASE_IMAGE=$BASE_IMAGE \
-		--build-arg PYTORCH_IMAGE=l4t-pytorch:r$L4T_VERSION-pth1.11-py3 \
-		--build-arg TENSORFLOW_IMAGE=l4t-tensorflow:r$L4T_VERSION-tf1.15-py3 \
+		--build-arg PYTORCH_IMAGE=nvcr.io/nvidia/l4t-pytorch:r$L4T_VERSION-pth1.11-py3 \
+		--build-arg TENSORFLOW_IMAGE=nvcr.io/nvidia/l4t-tensorflow:r$L4T_VERSION-tf2.9-py3 \
 		--build-arg PYTHON3_VERSION=$PYTHON3_VERSION \
 		--build-arg OPENCV_URL=$OPENCV_URL \
 		--build-arg OPENCV_DEB=$OPENCV_DEB \
