acd1a3426a527d1ec7c907dd1bf45d024ab2f2b7
Mekala02 <ernkla@gmail.com>
2022-10-15 15:56:40 +0300

    add serial communication

--- parts/arduino/arduino.ino
+++ parts/arduino/arduino.ino
@@ -14,7 +14,13 @@
 #define STEERING_MID_VALUE 1540
 #define STEERING_FULL_LEFT_VALUE 1900
 
+String pyserial_data;
+float pyserial_throttle;
+float pyserial_steering;
+float speed = 0;
+
 int readChannel(int channelInput, int defaultValue);
+float mapfloat(float x, float in_min, float in_max, float out_min, float out_max);
 
 Servo throttle;
 Servo steering;
@@ -24,16 +30,58 @@ void setup() {
     pinMode(CH2_PIN, INPUT);
     throttle.attach(ESC_PIN);
     steering.attach(Servo_PIN);
-    Serial.begin(9600);
+    Serial.begin(115200);
 }
 
 float steering_value, throttle_value;
 
 void loop() {
-    steering_value = readChannel(CH1_PIN, STEERING_MID_VALUE);
-    throttle_value = readChannel(CH2_PIN, 0);
-    steering.writeMicroseconds(steering_value);
+    // If value equals to 10 that means we are in manuel mode
+    // Received steering and throttle values are between -1 and 1
+    // We have to convert them to pwm
+    // multiplying with 1 for reversing left and right
+    if (pyserial_throttle == 10)
+        throttle_value = readChannel(CH2_PIN, 0);
+    else if (pyserial_throttle < 1 && pyserial_throttle > -1)
+        throttle_value = readChannel(CH2_PIN, 0); // tmp for safety
+        // throttle_value = mapfloat(-pyserial_throttle, -1, 1, THROTTLE_FORWARD_PWM, THROTTLE_REVERSE_PWM); // not tested
+    // else
+    //     throttle_value = THROTTLE_STOPPED_PWM // not tested
+
+    if (pyserial_steering == 10)
+        steering_value = readChannel(CH1_PIN, STEERING_MID_VALUE);
+    else if (pyserial_steering < 1 && pyserial_steering > -1)
+        steering_value = mapfloat(-pyserial_steering, -1, 1, STEERING_FULL_RIGHT_VALUE, STEERING_FULL_LEFT_VALUE);
+
     throttle.writeMicroseconds(throttle_value);
+    steering.writeMicroseconds(steering_value);
+
+    Serial.println("t" + String(mapfloat(throttle_value, 900, 2000, -1, 1)) + "s" + String(mapfloat(steering_value, 900, 2000, -1, 1)) + "v" + String(speed) + "e");
+
+	if(Serial.available() > 0) {
+		pyserial_data =  Serial.readStringUntil('\r');
+		// Serial.print(pyserial_data);
+	}
+
+    int deliminator_index = 0;
+    if (pyserial_data[0] == 't'){
+        for (int i = 1; i < pyserial_data.length(); i++) {
+            if (pyserial_data[i] == 's'){
+                pyserial_throttle = pyserial_data.substring(1, i).toFloat();
+                // Serial.print(pyserial_throttle);
+                deliminator_index = i;
+            }
+            else if (pyserial_data[i] == 'e'){
+                pyserial_steering = pyserial_data.substring(deliminator_index+1, i).toFloat();
+                break;
+            }
+        }
+    }
+}
+
+float mapfloat(float x, float in_min, float in_max, float out_min, float out_max)
+{
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
 int readChannel(int channelInput, int defaultValue){
--- parts/arduino/arduino.py
+++ parts/arduino/arduino.py
@@ -1,19 +1,79 @@
+import threading
 import logging
+import serial
+import time
+import re
 
 logger = logging.getLogger(__name__)
 
 class Arduino:
     def __init__(self):
-        self.threaded = False
+        self.threaded = True
         self.memory = 0
         self.run = True
         self.outputs = {"Speed": 0}
+        self.Steering = 0
+        self.Throttle = 0
+        self.Speed = 0
+        self.arduino_data_thread = threading.Thread(target=lambda a: print("a"))
         # If pilot = manuel self.outputs = {"speed": 0, "steering": 0, "throttle": 0}
+        self.arduino = serial.Serial(port='/dev/ttyUSB0', baudrate=115200, timeout=0.006, write_timeout=0.006)
         logger.info("Successfully Added")
 
+    def start_thread(self):
+        logger.info("Starting Thread")
+        buffer = False
+        while self.run:
+            while (self.arduino.in_waiting > 0):
+                buffer = self.arduino.readline().decode('utf-8')
+            # print(data)
+            # Looking if data is right format
+            if buffer and buffer[0] == "t":
+                # re.search(r"t\d+.\d+s\d+.\d+v\d+.\d+e", data)
+                data_array = re.split(r't|s|v|e', buffer)
+                self.Throttle = float(data_array[1])
+                self.Steering = float(data_array[2])
+                self.Speed = float(data_array[3])
+
+            time.sleep(0.003)
+
     def update(self):
-        pass
+        self.memory.memory["Speed"] = self.Speed
+        if self.memory.memory["Pilot_Mode"] == "Angle":
+            self.memory.memory["Throttle"] = self.Throttle
+        elif self.memory.memory["Pilot_Mode"] == "Manuel":
+            self.memory.memory["Throttle"] = self.Throttle
+            self.memory.memory["Steering"] = self.Steering
+
+        # We want to send the data as fast as we can so we are only sending 2 float, that
+        # floats encode motor power and drive mode parameters to throttle and steering values
+        # If that float is -1 < float < 1 arduino will use that value to drive motors
+        # If float is = 10 arduino won't use that values for controlling the motors 
+        pilot_mode_string = self.memory.memory["Pilot_Mode"]
+        motor_power = self.memory.memory["Motor_Power"]
+        if pilot_mode_string == "Manuel":
+            throttle = 10
+            steering = 10
+        elif pilot_mode_string == "Angle":
+            throttle = 10
+            steering = self.memory.memory["Steering"]
+        elif pilot_mode_string == "Full_Auto":
+            throttle = self.memory.memory["Throttle"] * motor_power
+            steering = self.memory.memory["Steering"]
+
+        # t is for stating start of throttle value s is for steering and e is for end, \r for read ending
+        formatted_data = "t" + str(throttle) + "s" + str(steering) + 'e' + '\r'
+
+        try:
+            self.arduino.write(formatted_data.encode())
+        except Exception as e:
+            # print(e)
+            pass
+        else:
+            # print("succes!!!")
+            pass
 
     def shut_down(self):
         self.run = False
-        logger.info("Successfully Added")
+        self.arduino.close()
+        logger.info("Stopped")
