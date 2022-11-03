859ebe75b67fc32ef6ea2a3182eaf4dade770834
Mekala02 <ernkla@gmail.com>
2022-11-03 15:49:17 +0300

    add early stop

--- parts/pilot/train.py
+++ parts/pilot/train.py
@@ -50,14 +50,14 @@ def main():
     train_set_loader = DataLoader(dataset=train_set, batch_size=batch_size, shuffle=True)
     test_set_loader = DataLoader(dataset=test_set, batch_size=batch_size, shuffle=True)
 
-    trainer = Trainer(model, criterion, optimizer, device, num_epochs, train_set_loader, test_set_loader=test_set_loader, use_other_inputs=False)
+    trainer = Trainer(model, criterion, optimizer, device, num_epochs, train_set_loader, test_set_loader=test_set_loader, use_other_inputs=False, patience=3, delta=0.0005)
     trainer.fit()
     # Saving the model
     # trainer.save_model()
 
 
 class Trainer:
-    def __init__(self, model, criterion, optimizer, device, num_epochs, train_set_loader, test_set_loader=None, use_other_inputs=False):
+    def __init__(self, model, criterion, optimizer, device, num_epochs, train_set_loader, test_set_loader=None, use_other_inputs=False, patience=3, delta=0.0005):
         self.model = model
         self.criterion = criterion
         self.optimizer = optimizer
@@ -66,6 +66,10 @@ class Trainer:
         self.test_set_loader = test_set_loader
         self.use_other_inputs = use_other_inputs
         self.num_epochs = num_epochs
+        # Delta: Minimum change to qualify as an improvement.
+        # Patience:  How many times we wait for change < delta before stop training.
+        self.patience = patience
+        self.delta = delta
 
         self.nu_of_train_batches = len(self.train_set_loader)
         if self.test_set_loader:
@@ -77,10 +81,12 @@ class Trainer:
         # torch.autograd.set_detect_anomaly(True)
         # logger.info(self.model)
         logger.info(f"Trainig on {self.device}...")
+        epoch_steering_losses = []
+        epoch_throttle_losses = []
         for epoch in range(1, self.num_epochs+1):
             self.model.train()
-            steering_losses = []
-            throttle_losses = []
+            batch_steering_losses = []
+            batch_throttle_losses = []
             pbar = tqdm(self.train_set_loader, desc=f"Epoch: {epoch}", file=sys.stdout, bar_format='{desc}{percentage:3.0f}%|{bar:100}')
             for batch_no, data in enumerate(pbar, 1):
                 self.optimizer.zero_grad()
@@ -98,12 +104,12 @@ class Trainer:
                     steering_prediction, throttle_prediction = self.model(images, other_inputs)
                 else:
                     steering_prediction, throttle_prediction = self.model(images)
-                steering_loss = self.criterion(steering_prediction, steering_labels)
-                throttle_loss = self.criterion(throttle_prediction, throttle_labels)
-                total_loss = steering_loss + throttle_loss
-                steering_losses.append(steering_loss.item())
-                throttle_losses.append(throttle_loss.item())
-                logger.info(f"\nBatch[{batch_no}/{self.nu_of_train_batches}] Total Loss: {total_loss:.2e}, Steering Loss: {steering_loss:.2e}, Throttle Loss: {throttle_loss:.2e}")
+                batch_steering_loss = self.criterion(steering_prediction, steering_labels)
+                batch_throttle_loss = self.criterion(throttle_prediction, throttle_labels)
+                total_loss = batch_steering_loss + batch_throttle_loss
+                batch_steering_losses.append(batch_steering_loss.item())
+                batch_throttle_losses.append(batch_throttle_loss.item())
+                logger.info(f"\nBatch[{batch_no}/{self.nu_of_train_batches}] Total Loss: {total_loss:.2e}, Steering Loss: {batch_steering_loss:.2e}, Throttle Loss: {batch_throttle_loss:.2e}")
                 # Backward
                 total_loss.backward()
                 # Gradient clipping
@@ -111,10 +117,12 @@ class Trainer:
                 # Gradient Descent Step
                 self.optimizer.step()
             # After finishing epoch we evaluating the model
-            epoch_steering_loss = sum(steering_losses) / len(steering_losses)
-            epoch_throttle_loss = sum(throttle_losses) / len(throttle_losses)
-            epoch_total_loss = steering_loss + throttle_loss
-            loss_string = f"For Epoch {epoch} --> Total Loss: {epoch_steering_loss}, Steering Loss: {epoch_throttle_loss:.2e}, Throttle Loss: {epoch_total_loss:.2e}"
+            epoch_steering_loss = sum(batch_steering_losses) / len(batch_steering_losses)
+            epoch_throttle_loss = sum(batch_throttle_losses) / len(batch_throttle_losses)
+            epoch_total_loss = epoch_steering_loss + epoch_throttle_loss
+            epoch_steering_losses.append(epoch_steering_loss)
+            epoch_throttle_losses.append(epoch_throttle_loss)
+            loss_string = f"For Epoch {epoch} --> Total Loss: {epoch_steering_loss:.2e}, Steering Loss: {epoch_throttle_loss:.2e}, Throttle Loss: {epoch_total_loss:.2e}"
             if self.test_set_loader:
                 logger.info("Evaluating on test set ...")
                 eval_total_loss, eval_steering_loss, eval_throttle_loss = self.evaluate()
@@ -132,6 +140,15 @@ class Trainer:
                 if eval_total_loss < self.test_set_min_loss:
                     self.test_set_min_loss = eval_total_loss
                     self.save_model()
+
+            # Early stopping
+            if batch_no >= self.patience:
+                last_index = batch_no-1
+                epochs_total_losses = sum(epoch_steering_losses) + sum(epoch_throttle_losses)
+                # If loss improve smaller than delta for patience times stops training 
+                if (epochs_total_losses[last_index] - epochs_total_losses[last_index-1] < self.delta) and (epochs_total_losses[last_index-1] - epochs_total_losses[last_index-2] < self.delta):
+                    logger.info(f"Stopped Training: Delta Loss Is Smaller Than {self.delta} For {self.patience} Times")
+                    break
 
     def evaluate(self):
         logger.info("Evaluation Mode")
@@ -168,6 +185,7 @@ class Trainer:
         torch.save(self.model.state_dict(), model_save_path)
         script_cell = torch.jit.script(self.model)
         torch.jit.save(script_cell, jit_model_save_path)
+        logger.info(f"Saved the model to {model_save_path}")
 
 
 if __name__ == "__main__":
