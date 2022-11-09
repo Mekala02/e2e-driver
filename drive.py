ce2504e74aa42f084052dfe91e45182826f39853
Mekala02 <ernkla@gmail.com>
2022-11-09 14:22:23 +0300

    add safety

--- drive.py
+++ drive.py
@@ -23,42 +23,42 @@ import logging
 import sys
 import time
 
+if __name__ == '__main__':
+    args = docopt(__doc__)
+    logging.basicConfig(level=logging.INFO, format="%(levelname)s: @ %(name)s %(message)s")
+    logger = logging.getLogger("drive")
 
-args = docopt(__doc__)
-logging.basicConfig(level=logging.INFO, format="%(levelname)s: @ %(name)s %(message)s")
-logger = logging.getLogger("drive")
+    vehicle_memory = Memory()
+    vehicle = Vehicle(vehicle_memory)
 
-vehicle_memory = Memory()
-vehicle = Vehicle(vehicle_memory)
+    model_folder_path = args["<model>"]
+    if model_folder_path:
+        vehicle_memory.untracked["Model_Folder_Path"] = model_folder_path
 
-model_folder_path = args["<model>"]
-if model_folder_path:
-    vehicle_memory.untracked["Model_Folder_Path"] = model_folder_path
+    logger.info("Adding parts to vehicle ... \n")
+    vehicle.add(Data_Logger(vehicle_memory))
+    vehicle.add(Camera_IMU(vehicle_memory))
+    if cfg["USE_OBJECT_DETECTION"]:
+        vehicle.add(Yolo(vehicle_memory))
+    if cfg["USE_COPILOT"]:
+        vehicle.add(Copilot(vehicle_memory))
+    vehicle.add(Pilot(vehicle_memory))
+    vehicle.add(Arduino(vehicle_memory))
+    vehicle.add(Web_Server(vehicle_memory))
+    vehicle.add(FPS_Counter(vehicle_memory))
 
-logger.info("Adding parts to vehicle ... \n")
-vehicle.add(Data_Logger(vehicle_memory))
-vehicle.add(Camera_IMU(vehicle_memory))
-if cfg["USE_OBJECT_DETECTION"]:
-    vehicle.add(Yolo(vehicle_memory))
-if cfg["USE_COPILOT"]:
-    vehicle.add(Copilot(vehicle_memory))
-vehicle.add(Pilot(vehicle_memory))
-vehicle.add(Arduino(vehicle_memory))
-vehicle.add(Web_Server(vehicle_memory))
-vehicle.add(FPS_Counter(vehicle_memory))
+    vehicle.start()
 
-vehicle.start()
-
-logger.info("Starting the drive loop \n")
-rate_hz = cfg["DRIVE_LOOP_HZ"]
-try:
-    while True:
-        start_time = time.time()
-        vehicle.update()
-        sleep_time = 1.0 / rate_hz - (time.time() - start_time)
-        if sleep_time > 0.0:
-            time.sleep(sleep_time)
-except KeyboardInterrupt:
-    vehicle.shut_down()
-    logger.info("Vehicle Shut Down \n")
-    sys.exit()
+    logger.info("Starting the drive loop \n")
+    rate_hz = cfg["DRIVE_LOOP_HZ"]
+    try:
+        while True:
+            start_time = time.time()
+            vehicle.update()
+            sleep_time = 1.0 / rate_hz - (time.time() - start_time)
+            if sleep_time > 0.0:
+                time.sleep(sleep_time)
+    except KeyboardInterrupt:
+        vehicle.shut_down()
+        logger.info("Vehicle Shut Down \n")
+        sys.exit()
