bf6d51b40836639e2788ae53ff572685d9360b39
Mekala02 <ernkla@gmail.com>
2022-11-10 12:16:36 +0300

    Add overwrite support

--- parts/memory.py
+++ parts/memory.py
@@ -6,6 +6,11 @@ class Memory:
         self.big_memory = {}
         # Stores data like save folder name
         self.untracked = {}
+        # If some part wants to change value but that value is not its output we add
+        # that change to overwrite then when owner that value see the overwrite value
+        # It will change the value accordingly
+        # Use pop when reading inside owner !!! Othervise it will always overwrite last overwrite value (pop deletes it)
+        self.overwrite = {}
 
     def add(self, name, value=0):
         self.memory[name] = value
