afab6e90296eb55d38e5ea0e5d5019efeb6b6cd1
Mekala02 <ernkla@gmail.com>
2022-11-11 12:27:35 +0300

    Faster Loop

--- parts/arduino/arduino.ino
+++ parts/arduino/arduino.ino
@@ -1,3 +1,11 @@
+/*
+    When pulseIn command is waiting for input main loop is blocked hence our loop will be slow.
+    We can use interrupts for faster loop (interrupts are non blocking).
+    But arduino Nano only supports 2 pin for interrupts (2, 3). Since we have 3 channels + 1 channel for encoder = 4 we have to use
+    pulseIn. If ypu want to get true 100+ fps you have to use Arduino Micro it has 5 interrupt usable pin.
+
+*/
+
 #include <Servo.h>
 #include <Ewma.h>
 
@@ -9,7 +17,7 @@
 #define ENCODER_PIN 2
 
 // Rc receiver filters (Exponentially Weighted Moving Average)
-Ewma Steering_Filter(0.25);
+Ewma Steering_Filter(0.5);
 Ewma Thorttle_Filter(0.2);
 Ewma Encoder_Filter(0.15);
 
@@ -20,6 +28,11 @@ String pyserial_data;
 int pyserial_throttle, pyserial_steering;
 int steering_value, throttle_value, ch3_value, mode_button;
 
+volatile long Ch1_StartTime = 0;
+volatile long Ch1_CurrentTime = 0;
+volatile long Ch1_Pulses = 1500;
+int Ch1_PulseWidth = 0;
+
 unsigned long current_time;
 unsigned long serialOut_last_time;
 unsigned long encoder_last_time;
@@ -28,14 +41,17 @@ float dpulses = 0;
 
 
 int readChannel(int channelInput, int defaultValue);
+void Ch1_PulseTimer();
 
 void setup() {
     pinMode(CH1_PIN, INPUT);
     pinMode(CH2_PIN, INPUT);
+    pinMode(CH3_PIN, INPUT);
+    pinMode(ENCODER_PIN, INPUT);
     throttle.attach(ESC_PIN);
     steering.attach(Servo_PIN);
-    pinMode(ENCODER_PIN, INPUT);
-    attachInterrupt(digitalPinToInterrupt(ENCODER_PIN), []{pulses++;}, FALLING );
+    attachInterrupt(digitalPinToInterrupt(CH1_PIN), Ch1_PulseTimer, CHANGE);
+    attachInterrupt(digitalPinToInterrupt(ENCODER_PIN), []{pulses++;}, FALLING);
     Serial.begin(115200);
 }
 
@@ -61,18 +77,18 @@ void loop() {
     }
 
     // If value equals to 0 that means we are in manuel mode
-    if (pyserial_steering == 0)
-        steering_value = Steering_Filter.filter(readChannel(CH1_PIN, 1500));
+    if (pyserial_steering == 0 && Ch1_Pulses < 2000)
+        steering_value = Steering_Filter.filter(Ch1_Pulses);
     else if (900 < pyserial_steering && pyserial_steering < 2100)
         steering_value = pyserial_steering;
     
     if (pyserial_throttle == 0)
         throttle_value = Thorttle_Filter.filter(readChannel(CH2_PIN, 0));
     else if (900 < pyserial_throttle && pyserial_throttle < 2100)
-        throttle_value = readChannel(CH2_PIN, 0); // tmp for safety
+        throttle_value = 1500; // tmp for safety
         // throttle_value = pyserial_throttle; // not tested
     // else
-    //     throttle_value = THROTTLE_STOPPED_PWM // not tested
+    //     throttle_value = 1500 // not tested
 
     ch3_value = readChannel(CH3_PIN, 0);
     if (ch3_value > 500 && ch3_value < 1500)
@@ -93,9 +109,17 @@ void loop() {
     }
 
     // Sending data 200fps
-    if (current_time - serialOut_last_time > 5){
+    if (current_time - serialOut_last_time > 0){
         Serial.println("s" + String(steering_value) + "t" + String(throttle_value) + "m" + String(mode_button) + "v" + String(dpulses) + "e");
         serialOut_last_time = current_time;
+    }
+}
+
+void Ch1_PulseTimer(){
+    Ch1_CurrentTime = micros();
+    if (Ch1_CurrentTime > Ch1_StartTime){
+        Ch1_Pulses = Ch1_CurrentTime - Ch1_StartTime;
+        Ch1_StartTime = Ch1_CurrentTime;
     }
 }
 
