f6ed3c7d69488a271636f2abf2ed224e4082711f
Mekala02 <ernkla@gmail.com>
2022-11-11 18:24:29 +0300

    tensorboard support

--- parts/pilot/train.py
+++ parts/pilot/train.py
@@ -11,6 +11,8 @@ from networks import Linear_With_Others
 from data_loader import Load_Data
 
 from torch.utils.data import DataLoader
+import torchvision
+from torch.utils.tensorboard import SummaryWriter
 from docopt import docopt
 from tqdm import tqdm
 import logging
@@ -24,6 +26,17 @@ logger = logging.getLogger("train")
 
 
 def main():
+    # Hyperparameters
+    reduce_fps = False
+    use_depth_input = False
+    use_other_inputs = False
+    #2e-3 for startup then reduce to 1e-3
+    learning_rate = 2e-3
+    batch_size = 1024
+    num_epochs = 100
+    test_data_percentage = 20
+    detailed_tensorboard = False
+
     args = docopt(__doc__)
     data_dirs = args["<data_dirs>"]
     model_path = args["--model"]
@@ -34,18 +47,10 @@ def main():
     torch.manual_seed(22)
     # If set to value like 30 it will make training data ~30fps 
     # It wont work great if datasets fps is close to reduce_fps
-    reduce_fps = False
-    use_depth_input = False
-    use_other_inputs = False
     if use_depth_input:
         in_channels = 4
     else:
         in_channels = 3
-    test_data_percentage = 20
-    #2e-3 for startup then reduce to 1e-3
-    learning_rate = 2e-3
-    batch_size = 1024
-    num_epochs = 100
 
     # Our input size is not changing so we can use cudnn's optimization
     torch.backends.cudnn.benchmark = True
@@ -55,6 +60,8 @@ def main():
         model.load_state_dict(torch.load(model_path))
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
     criterion = torch.nn.MSELoss()
+    writer = SummaryWriter("tensorboard")
+    writer.add_graph(model, input_to_model=torch.ones((1, 3, 120, 160), device=device), verbose=False, use_strict_trace=True)
 
     dataset = Load_Data(data_dirs, reduce_fps=reduce_fps, use_depth_input=use_depth_input, use_other_inputs=use_other_inputs)
     test_len = math.floor(len(dataset) * test_data_percentage / 100)
@@ -65,19 +72,34 @@ def main():
     # it will be slow if input size change (batch size is changing on last layer if data_set_len%batch_size!=0) so we set drop_last = True
     train_set_loader = DataLoader(dataset=train_set, batch_size=batch_size, shuffle=True, num_workers=4, pin_memory=True, drop_last=True)
     test_set_loader = DataLoader(dataset=test_set, batch_size=batch_size, num_workers=4, pin_memory=True)
-    trainer = Trainer(model, criterion, optimizer, device, num_epochs, train_set_loader, test_set_loader=test_set_loader, model_name=model_save_name, use_other_inputs=use_other_inputs, patience=5, delta=0.00005)
+
+    if detailed_tensorboard:
+        # Adding first batches of train and test images to tensorboard
+        data = next(iter(train_set_loader))
+        images = data[0]
+        grid = torchvision.utils.make_grid(images)
+        writer.add_image(f"Train Set First Batch", grid, 0)
+        if test_set_loader:
+            data = next(iter(test_set_loader))
+            images = data[0]
+            grid = torchvision.utils.make_grid(images)
+            writer.add_image(f"Test Set First Batch", grid, 0)
+
+    trainer = Trainer(model, criterion, optimizer, device, num_epochs, train_set_loader, writer=writer, test_set_loader=test_set_loader, model_name=model_save_name, use_other_inputs=use_other_inputs, patience=5, delta=0.00005)
     trainer.fit()
+    writer.close()
     # Saving the model
     # trainer.save_model()
 
 
 class Trainer:
-    def __init__(self, model, criterion, optimizer, device, num_epochs, train_set_loader, test_set_loader=None, model_name="model", use_other_inputs=False, patience=3, delta=0.0005):
+    def __init__(self, model, criterion, optimizer, device, num_epochs, train_set_loader, writer=None, test_set_loader=None, model_name="model", use_other_inputs=False, patience=3, delta=0.0005):
         self.model = model
         self.criterion = criterion
         self.optimizer = optimizer
         self.device = device
         self.train_set_loader = train_set_loader
+        self.writer = writer
         self.test_set_loader = test_set_loader
         self.model_name = model_name
         self.use_other_inputs = use_other_inputs
@@ -145,12 +167,20 @@ class Trainer:
             epoch_throttle_losses.append(epoch_throttle_loss)
             epoch_losses.append(epoch_loss)
             loss_string = f"For Epoch {epoch} --> Loss: {epoch_loss:.4f}, Steering Loss: {epoch_steering_loss:.4f}, Throttle Loss: {epoch_throttle_loss:.4f}"
+            if self.writer:
+                self.writer.add_scalar('Train/Loss', epoch_loss, epoch)
+                self.writer.add_scalar('Train/Steering_Loss', epoch_steering_loss, epoch)
+                self.writer.add_scalar('Train/Throttle_Loss', epoch_throttle_loss, epoch)
             if self.test_set_loader:
                 logger.info("\nEvaluating on test set ...")
                 eval_loss, eval_steering_loss, eval_throttle_loss = self.evaluate()
                 val_loss_string = f"Val Loss: {eval_loss:.4f} Steering Val Loss: {eval_steering_loss:.4f}, Throttle Val Loss: {eval_throttle_loss:.4f}"
                 logger.info(val_loss_string + " - " + loss_string)
                 logger.info(f"PWM Differance: +-{math.sqrt(eval_loss) * 500}\n")
+                if self.writer:
+                    self.writer.add_scalar('Test/Loss', eval_loss, global_step=epoch)
+                    self.writer.add_scalar('Test/Steering_Loss', eval_steering_loss, global_step=epoch)
+                    self.writer.add_scalar('Test/Throttle_Loss', eval_throttle_loss, global_step=epoch)
             else:
                 logger.info(loss_string + '\n')
 
@@ -180,6 +210,8 @@ class Trainer:
             if (self.train_not_improved_count >= self.patience):
                 logger.info(f"Stopped Training: Delta Loss Is Smaller Than {self.delta} For {self.patience} Times")
                 break
+            if self.writer:
+                self.writer.flush()
 
     def evaluate(self):
         self.model.eval()
