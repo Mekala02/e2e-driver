05835f097d2ea5051bfe171b760770f4e669df33
Mekala02 <ernkla@gmail.com>
2022-11-16 10:00:59 +0300

    Switch to ppm

---
+++ parts/arduino/arduino.ino
@@ -0,0 +1,109 @@
+#include <Servo.h>
+#include <Ewma.h>
+
+#define PPM_PIN 3
+#define ESC_PIN 9
+#define Servo_PIN 10
+#define ENCODER_PIN 2
+
+// Exponentially Weighted Moving Average Filter
+Ewma Encoder_Filter(0.15);
+
+Servo ESC;
+Servo SERVO;
+
+String pyserial_data;
+int pyserial_throttle, pyserial_steering;
+int steering, throttle, mode_button;
+
+// current time, previous time, channel no, time differance
+unsigned long int t, tp, c, d = 0;
+int ch[15];
+
+unsigned long current_time;
+unsigned long serialOut_last_time;
+unsigned long encoder_last_time;
+unsigned int pulses = 0;
+float dpulses = 0;
+
+void Read_Channels();
+
+void setup() {
+    pinMode(PPM_PIN, INPUT_PULLUP);
+    pinMode(ENCODER_PIN, INPUT);
+    ESC.attach(ESC_PIN);
+    SERVO.attach(Servo_PIN);
+    attachInterrupt(digitalPinToInterrupt(PPM_PIN), Read_Channels, FALLING);
+    attachInterrupt(digitalPinToInterrupt(ENCODER_PIN), []{pulses++;}, FALLING);
+    Serial.begin(115200);
+}
+
+void loop() {
+    current_time = millis();
+    // Reading serial input
+    // Serial inputs shape: s(float)t(float)e
+    if(Serial.available() > 0) {
+        pyserial_data =  Serial.readStringUntil('\r');
+        int deliminator_index = 0;
+        if (pyserial_data[0] == 's'){
+            for (int i = 1; i < pyserial_data.length(); i++) {
+                if (pyserial_data[i] == 't'){
+                    pyserial_steering = pyserial_data.substring(1, i).toInt();
+                    deliminator_index = i;
+                }
+                else if (pyserial_data[i] == 'e'){
+                    pyserial_throttle = pyserial_data.substring(deliminator_index+1, i).toInt();
+                    break;
+                }
+            }
+        }
+    }
+
+    // If value equals to 0 that means we are in manuel mode
+    if (pyserial_steering == 0 && ch[1] < 2000)
+        steering = ch[1];
+    else if (1000 < pyserial_steering && pyserial_steering < 2000)
+        steering = pyserial_steering;
+    
+    if (pyserial_throttle == 0)
+        throttle = ch[3];
+    else if (1000 < pyserial_throttle && pyserial_throttle < 2000)
+        throttle = 1500; // tmp for safety
+        // throttle = pyserial_throttle; // not tested
+    // else
+    //     throttle = 1500 // not tested
+
+    if (ch[3] > 500 && ch[3] < 1500)
+        mode_button = 1;
+    else if (ch[3] > 1500 && ch[3] < 2100)
+        mode_button = 2;
+    else
+        mode_button = 0;
+
+    SERVO.writeMicroseconds(steering);
+    ESC.writeMicroseconds(throttle);
+
+    // Calculating speed at certain rate
+    if (current_time - encoder_last_time > 10){
+        dpulses = Encoder_Filter.filter(1000 * pulses / (current_time - encoder_last_time));
+        pulses = 0;
+        encoder_last_time = current_time;
+    }
+
+    // Sending data 100fps
+    if (current_time - serialOut_last_time > 10){
+        Serial.println("s" + String(steering) + "t" + String(throttle) + "m" + String(mode_button) + "v" + String(dpulses) + "e");
+        serialOut_last_time = current_time;
+    }
+}
+
+// For reading ppm signal
+void Read_Channels(){
+    t = micros();
+    d = t - tp;
+    if (d > 3000)
+        c = 0;
+    ch[c] = d;
+    c ++;
+    tp = t;
+}
