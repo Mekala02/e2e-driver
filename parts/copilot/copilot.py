08111c26400d236b93f8ebf1be2e33471a303445
Mekala02 <ernkla@gmail.com>
2022-11-16 10:59:55 +0300

    Direction change according to receiver

--- parts/copilot/copilot.py
+++ parts/copilot/copilot.py
@@ -9,19 +9,16 @@ class Copilot:
         self.thread = None
         self.run = True
         self.outputs = {"Stop": 0, "Taxi": 0, "Direction": "Right", "Lane": "Right"}
-
-        self.directions = ["Left", "Forward", "Right"]
-        self.lanes = ["Left", "Right"]
-        self.counter = 0
         logger.info("Successfully Added")
 
     def update(self):
-        self.counter += 1
-        if self.counter%1000 == 1:
-            self.memory.memory["Direction"] = random.choice(self.directions)
-            self.memory.memory["Stop"] = random.randint(0, 1)
-            self.memory.memory["Taxi"] = random.randint(0, 1)
-            self.memory.memory["Lane"] = random.choice(self.lanes)
+        mode = self.memory.memory["Mode2"]
+        if mode == 1:
+            self.memory.memory["Direction"] = "Left"
+        if mode == 2:
+            self.memory.memory["Direction"] = "Forward"
+        if mode == 3:
+            self.memory.memory["Direction"] = "Right"
 
     def shut_down(self):
         self.run = False
