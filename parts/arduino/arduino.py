d544bcf06afb3795ba99bb6e7394db8a455cd358
Mekala02 <ernkla@gmail.com>
2022-11-29 11:30:36 +0300

    better comments

--- parts/arduino/arduino.py
+++ parts/arduino/arduino.py
@@ -16,13 +16,13 @@ class Arduino:
         self.thread = "Single"
         self.run = True
         self.outputs = {"Speed": 0, "Mode1": 0, "Mode2": 0}
+        # It can output steering and throttle if pilot equals to manuel
         # Also It can output Record and drive mode acoording to mode button 
         self.Steering = 0
         self.Throttle = 0
         self.Mode1 = 0
         self.Mode2 = 0
         self.Speed = 0
-        # If pilot = manuel self.outputs = {"speed": 0, "steering": 0, "throttle": 0}
         self.arduino = serial.Serial(port='/dev/ttyUSB0', baudrate=115200, timeout=0.006, write_timeout=0.006)
         time.sleep(0.04)
         logger.info("Successfully Added")
@@ -40,7 +40,7 @@ class Arduino:
             # if 0 --> radio turned off, elif 1 --> mode 1, elif 2 --> mode 2
             self.Mode1 = int(data_array[3])
             self.Mode2 = int(data_array[4])
-            # data_array[3] sends ticks/sec we convert it to cm/sec
+            # data_array[3] is ticks/sec we converting it to cm/sec
             self.Speed = float(data_array[5]) / cfg["TICKS_PER_CM"]
 
     def start_thread(self):
@@ -48,14 +48,13 @@ class Arduino:
         while self.run:
             start_time = time.time()
             self.grab_data()
-            # Arduino sends data @100 fps so we receiving it @120fps
+            # Arduino sends data @100hz we limiting the thread speed to @120hz
             sleep_time = 1.0 / 120 - (time.time() - start_time)
             if sleep_time > 0.0:
                 time.sleep(sleep_time)
 
     def update(self):
         # self.Throttle, self.Steering and self.Speed values comes from arduino
-        # We writing or not writing those values to memory acording to driving mode
         pilot_mode_string = self.memory.memory["Pilot_Mode"]
         if pilot_mode_string == "Angle":
             self.memory.memory["Throttle"] = self.Throttle
@@ -67,8 +66,8 @@ class Arduino:
         self.memory.memory["Mode2"] = self.Mode2
         # We want to send the data as fast as we can so we are only sending 2 int as string, those
         # strings encode motor power and drive mode parameters to throttle and steering values
-        # If char is 1000 < char < 2000 arduino will use that value to drive motors
-        # If char is = 0 arduino won't use that values for controlling the motors
+        # If char is between 1000, 2000 arduino will use that value to drive motors
+        # If char is = 0 arduino won't use that value for controlling the actuator
         motor_power = self.memory.memory["Motor_Power"]
         if pilot_mode_string == "Manuel":
             steering = 0
@@ -79,12 +78,12 @@ class Arduino:
         elif pilot_mode_string == "Full_Auto":
             steering = self.memory.memory["Steering"]
             throttle = self.memory.memory["Throttle"] * motor_power
-        # s is for stating start of throttle value t is for steering and e is for end, \r for read ending
+        # s is for stating start of steering value t is for throttle and e is for end, \r for read ending
         formatted_data = "s" + str(steering) + "t" + str(throttle) + 'e' + '\r'
         try: self.arduino.write(formatted_data.encode())
         except Exception as e: pass # logger.warning(e)
         else: pass # logger.info("Succes !!!")
-            
+
     def shut_down(self):
         self.run = False
         # If we reading from arduino and close the port that
