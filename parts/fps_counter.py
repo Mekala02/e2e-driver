c945acb937b4fcf64c73a41159be2113d9400a93
Mekala02 <ernkla@gmail.com>
2022-12-01 10:30:52 +0300

    variable name change

--- parts/fps_counter.py
+++ parts/fps_counter.py
@@ -9,18 +9,17 @@ class FPS_Counter:
         self.run = True
         self.outputs = {"Fps": 0}
         
-        self.past_time = 0
+        self.prev_time = 0
         self.counter = 0
         logger.info("Successfully Added")
 
     def update(self):
         # Converting ns to ms
         timestamp = self.memory.memory["Timestamp"] * 1e-6
-        if timestamp - self.past_time > 1000:
+        if timestamp - self.prev_time > 1000:
             self.memory.memory["Fps"] = self.counter
-            # self.fps_list.append(self.counter)
             self.counter = 0
-            self.past_time = timestamp
+            self.prev_time = timestamp
         else:
             self.counter += 1
 
