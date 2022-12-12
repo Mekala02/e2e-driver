bed5a2375b86063156afbf153fd292c89b8b45c0
Mekala02 <ernkla@gmail.com>
2022-12-12 18:47:35 +0300

    PID Support

--- common_functions.py
+++ common_functions.py
@@ -1,4 +1,5 @@
 import logging
+import time
 logger = logging.getLogger(__name__)
 
 class Image_Loader():
@@ -59,6 +60,42 @@ class Image_Loader():
         if self.img_format == "svo":
             return self.f(path, zed_data_id, type_)
         return self.f(path)
+
+class PID():
+    def __init__(self, Kp=0, Ki=0, Kd=0, I_max=1):
+        self.Kp = Kp
+        self.Ki = Ki
+        self.Kd = Kd
+        self.I_max = I_max
+        self.current_time = 0
+        self.previous_time = 0
+        self.current_error = 0
+        self.previous_error = 0
+        self.P = 0
+        self.I = 0
+        self.D = 0
+
+    def __call__(self, current_value, requested_value):
+        self.current_time = time.time()
+        delta_time = self.current_time - self.previous_time
+        self.current_error = requested_value - current_value
+        delta_error = self.current_error - self.previous_error
+
+        self.P = self.Kp * self.current_error
+        self.I += self.Ki * self.current_error * delta_time
+        self.D = self.Kd * delta_error / delta_time
+
+        # Integrator limit
+        if self.I > self.I_max: self.I = self.I_max
+        if self.I < -self.I_max: self.I = -self.I_max
+
+        # Integrator Reset
+        # if - 0.1 < self.current_error < 0.1: self.I = 0
+
+        self.previous_time = self.current_time
+        self.previous_error = self.current_error
+
+        return self.P + self.I + self.D
 
 def pwm2folat(value):
     return (value - 1500) / 500
